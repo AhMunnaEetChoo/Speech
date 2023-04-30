a838a74ef1082583e29b80e9e302f495e91f8ea2
Ollie <38665165+AhMunnaEetChoo@users.noreply.github.com>
2023-04-30 17:59:30 +0100

    Add the Intro scene

---
+++ Assets/IntroScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScript : MonoBehaviour
+{
+    private float m_timer = 0.0f;
+    public string m_nextScene = "MainScene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+        if(m_timer > 1.5f && Mathf.Abs(Input.GetAxis("Vertical")) > 0f)
+        {
+            SceneManager.LoadScene(m_nextScene, LoadSceneMode.Single);
+        }
+    }
+}
